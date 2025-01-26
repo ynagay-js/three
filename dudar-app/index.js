00124c8985f563249658821e06ec90955e25325d
ynagay-js <zgrepka@yandex.ru>
2025-01-26 22:45:06 +0300

    feat: add sphere and torus figure, add texture to cube figure

--- dudar-app/index.js
+++ dudar-app/index.js
@@ -23,25 +23,79 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // add renderer to html body output as html tag (domElement)
 document.body.appendChild(renderer.domElement);
 
-// create figure
+// create texture
+
+const texture = new THREE.TextureLoader().load('img/ducks.jpg');
+const textureMaterial = new THREE.MeshBasicMaterial({map: texture});
+
+// create cube figure
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 'green'});
+const material = new THREE.MeshBasicMaterial(textureMaterial);
 const cube = new THREE.Mesh(geometry, material);
 
-// add figure on scene
+// position cube on scene
+cube.position.set(-3, 0, 0);
+
+// add cube on scene
 scene.add(cube);
+
+//create sphere figure
+const sphereGeometry = new THREE.SphereGeometry(0.6, 40, 40);
+const sphereMaterial = new THREE.MeshPhongMaterial({
+    color: 'blue',
+    emissive: 'white',
+    shininess: 100
+})
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+// position sphere on scene
+sphere.position.set(3, 0, 0);
+
+// add sphere on scene
+scene.add(sphere);
+
+//create torus figure
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.7, 0.2, 16, 100),
+    new THREE.MeshBasicMaterial({color: 'green'})
+);
+
+// position torus on scene
+torus.position.set(0, 1.5, -1);
+
+// add torus on scene
+scene.add(torus);
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(3, 3),
+    textureMaterial
+);
+
+plane.position.set(0, -2, 0);
+scene.add(plane);
+
+
 
 // ongoing render
 
 function animate() {
     requestAnimationFrame(animate); //continuous function call
 
-    // figure rotate animation
+    // cube rotate animation
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    // sphere rotate animation
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+
+    // torus rotate animation
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.01;
 
     renderer.render(scene, camera);
 }
 
-//manual function call
+// manual function call
 animate();
