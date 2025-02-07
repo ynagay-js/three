a7b306d1ea7080feb8f19455e86b2f0fbac749e6
ynagay-js <zgrepka@yandex.ru>
2025-02-07 17:29:15 +0300

    feat: add cubes animation, use GSAP library on a sphere

--- simon-app/script.js
+++ simon-app/script.js
@@ -8,7 +8,7 @@ const scene = new THREE.Scene();
 
 const size = {
     width: 800,
-    height: 600
+    height: 800
 }
 
 const camera = new THREE.PerspectiveCamera(
@@ -31,6 +31,12 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setSize(size.width, size.height);
 
+// Group
+
+const group = new THREE.Group();
+group.scale.y = 2;
+scene.add(group);
+
 // Object
 
 const cube = new THREE.Mesh(
@@ -38,7 +44,94 @@ const cube = new THREE.Mesh(
     new THREE.MeshBasicMaterial({color: 'red'})
 )
 
-scene.add(cube);
+// console.log(cube.position.length());
+// console.log(cube.position.distanceTo(camera.position));
 
-renderer.render(scene, camera);
+// cube.scale.x = 0.5;
+// cube.scale.y = 2;
+// cube.scale.z = 0.25;
+
+cube.position.x = -2;
+cube.rotation.x = Math.PI * 0.25;
+cube.rotation.y = Math.PI * 0.25;
+
+// scene.add(cube);
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+cube2.position.x = 0;
+cube2.rotation.y = Math.PI * 0.25;
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({color: 'green'})
+)
+cube3.position.x = 2;
+cube3.rotation.y = Math.PI;
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.25),
+    new THREE.MeshBasicMaterial({color: 'white'})
+)
+sphere.position.set(-1, -0.75, 0);
+
+group.add(cube);
+group.add(cube2);
+group.add(cube3);
+group.add(sphere);
+// camera.lookAt(new THREE.Vector3(0, 1, 0));
+
+// Axes Helper
+
+// const axesHelper = new THREE.AxesHelper(2);
+// scene.add(axesHelper);
+
+
+// Animate 
+
+let time = Date.now();
+const clock = new THREE.Clock();
+
+// GSAP
+
+gsap.to(sphere.position, {
+    duration: 1,
+    delay: 1,
+    x: 1,
+    ease: "power1.inOut",
+    repeat: -1,
+    yoyo: true
+})
+
+// GSAP END
+
+const tick = () => {
+
+    window.requestAnimationFrame(tick);
+
+    const currentTime = Date.now();
+    const deltaTime = currentTime - time;
+    time = currentTime;
+
+    //clock
+    const elapsedTime = clock.getElapsedTime();
+
+    cube.rotation.x += 0.01 * deltaTime;
+    cube2.rotation.y = elapsedTime;
+    cube2.position.y = Math.cos(elapsedTime);
+    cube3.rotation.z += 0.01;
+    cube3.position.z = Math.sin(elapsedTime);
+
+    //animate camera positon
+    // camera.position.y = Math.sin(elapsedTime);
+    // camera.lookAt(group.position);
+
+    renderer.render(scene, camera);
+} 
+
+tick();
+
+
 
